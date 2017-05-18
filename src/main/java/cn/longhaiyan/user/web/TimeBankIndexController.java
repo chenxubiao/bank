fa7b3cba6921b35ba9f65d65e5a985b8d724cd3b
chenxubiao <chenxbubiaocxb@gmail.com>
2017-05-18 22:30:14 +0800

    modify:方便测试，增加系统角色为teacher：高育良

--- src/main/java/cn/longhaiyan/user/web/TimeBankIndexController.java
+++ src/main/java/cn/longhaiyan/user/web/TimeBankIndexController.java
@@ -1,10 +1,15 @@
 package cn.longhaiyan.user.web;
 
 
-import cn.longhaiyan.common.bean.UserSession;
+import cn.longhaiyan.common.utils.consts.BankConsts;
 import cn.longhaiyan.common.web.GuestBaseController;
 import cn.longhaiyan.user.bean.RegisterBean;
+import cn.longhaiyan.user.domain.Student;
+import cn.longhaiyan.user.domain.Teacher;
 import cn.longhaiyan.user.domain.UserInfo;
+import cn.longhaiyan.user.enums.UserTypeEnum;
+import cn.longhaiyan.user.service.StudentService;
+import cn.longhaiyan.user.service.TeacherService;
 import cn.longhaiyan.user.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
 import java.util.Map;
 
 @Controller
@@ -20,6 +26,8 @@ public class TimeBankIndexController extends GuestBaseController {
     private UserRegisterController registerController;
     @Autowired
     private UserInfoService userInfoService;
+    @Autowired
+    private TeacherService teacherService;
 
 
     @RequestMapping(value = {"index", "/"}, method = RequestMethod.GET)
@@ -27,6 +35,7 @@ public class TimeBankIndexController extends GuestBaseController {
 
         // TODO: 17-5-16
         int count = userInfoService.countAll();
+        UserInfo sys;
         if (count == 0) {
             RegisterBean registerBean = new RegisterBean();
             registerBean.setCode("abcde");
@@ -35,9 +44,23 @@ public class TimeBankIndexController extends GuestBaseController {
             registerBean.setPassword("abcde");
             registerController.regester(request, registerBean);
             System.out.println("系统账户已创建，name = " + registerBean.getUserName() + " ,psswd = " + registerBean.getPassword());
+            sys = userInfoService.findById(BankConsts.USER_IS_SYSTEM);
+            sys.setUserType(UserTypeEnum.TEACHER.getCode());
+            sys.setUserRole(BankConsts.CRM_ADMIN);
+            userInfoService.save(sys);
+
+            Teacher teacher = new Teacher();
+            teacher.setName("高育良");
+            teacher.setDept("汉东大学政法系主任");
+            teacher.setRemark("名师出高徒");
+            teacher.setCreateTime(new Date());
+            teacher.setModifyTime(teacher.getModifyTime());
+            teacher.setTno(110);
+            teacherService.save(teacher);
+            System.out.println("已授权用户角色为teacher：" + teacher.getName());
         }
-        UserInfo userInfo = userInfoService.findById(1);
-        super.setUserSession(request, userInfo);
+        sys = userInfoService.findById(BankConsts.USER_IS_SYSTEM);
+        super.setUserSession(request, sys);
 
         map.put("hello", "timebank");
         return "/index";
